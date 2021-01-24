75ffb5549705c9b1f1612761088ea3b5991214f5
Dennis Dunn <ansofive@gmail.com>
2021-01-23 23:01:53 -0700

    Added func to generate antigen administered doses from testcase history.

--- cl-cdsi.lisp
+++ cl-cdsi.lisp
@@ -2,5 +2,15 @@
 
 (in-package #:cl-cdsi)
 
+(defun split-dose (dose)
+  "Take a vaccine administered dose and create a list of antigen administered doses."
+  (let* ((cvx (gethash "cvx" dose))
+	 (date (gethash "dateAdministered" dose))
+	 (antigens (fetch "vaccines" cvx "antigens")))
+    (mapcar (lambda (antigen) (list "dateAdministered" date "antigen" antigen)) antigens)))
+
+(defun split-doses (doses)
+  (stable-sort (mapcan #'split-dose doses) #'string-lessp :key #'fourth))
+
 
 
