388cad15b84ce7d5e5d2541e3eb1b2e5918c097a
Dennis Dunn <ansofive@gmail.com>
2023-04-21 21:11:14 -0400

    Added simple tests for ehr-interface.

--- tests/ehr-interface.lisp
+++ tests/ehr-interface.lisp
@@ -1,1 +1,10 @@
 (in-package :cl-cdsi/tests/ehr-interface)
+
+(deftest test-the-enr-interface
+  (testing "fetch patient info by string id"
+    (let ((record (cl-cdsi/ehr-interface:get-patient-info "2013-0004")))
+      (ok (not (null record)))))
+
+  (testing "fetch patient info by numerical id"
+    (let ((record (cl-cdsi/ehr-interface:get-patient-info 20130004)))
+      (ok (not (null record))))))
