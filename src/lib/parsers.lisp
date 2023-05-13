f846762009334e84ba133ebb6f4fae20befdf4c9
Dennis Dunn <ansofive@gmail.com>
2023-05-12 21:48:18 -0400

    Finished the parsers.

--- src/lib/parsers.lisp
+++ src/lib/parsers.lisp
@@ -1,31 +1,58 @@
 (in-package :cdsi.lib)
 
-(defun parse-bool (x)
-  (not (or (null x)
-           (equal 0 x)
-           (equal "" x)
-           (equal "No" x)
-           (equal "N" x)
-           (equal "F" (format nil "~:@(~a~)" x))
-           (equal "FALSE" (format nil "~:@(~a~)" x)))))
+(defgeneric parse (text output-type &optional arg)
+  (:documentation "Parse the source text."))
 
-;;;; Parse dates "MM/DD/YYYY"
+(defmethod parse ((text string) (output-type (eql :string)) &optional (arg nil))
+  (declare (ignoreable output-type))
+  text)
 
-(defun parse-date (s)
-  "Parse a date of the form 'MM/DD/YYYY'"
-  (let* ((parts (ppcre:split "/" s))
-         (date-parts (mapcar #'parse-integer parts)))
-    (make-date (third date-parts) (first date-parts) (second date-parts))))
+(defmethod parse ((text string) (output-type (eql :number)) &optional (arg nil))
+  (declare (ignoreable output-type))
+  (read-from-string text))
 
-;;;; Parse intrvals "1 year - 4 days"
+(defmethod parse ((text string) (output-type (eql :bool)) &optional (arg nil))
+  (declare (ignoreable output-type))
+  (not (or (null text)
+           (equal 0 text)
+           (equal "" text)
+           (search text "NoFalse" :test #'char-equal))))
 
-(defun parse-interval (s)
-  "Return a list of intervals parsed from the string."
-  (if (or (string= "" s) (null s))
-      nil
-      (let ((str2 (ppcre:regex-replace-all "[sS\\s]" s ""))
-            result)
-        (ppcre:do-register-groups ((#'parse-integer value) (#'parse-keyword unit))
-          ("([+-]?\\d+)(\\w+)" str2)
-          (push (make-interval value unit) result))
-        (nreverse result))))
+(defmethod parse ((text number) (output-type (eql :bool)) &optional (arg nil))
+  (declare (ignoreable output-type))
+  (not (= 0 text)))
+
+(defmethod parse ((text string) (output-type (eql :common-date)) &optional (arg nil))
+  "MM/DD/YYYY"
+  (declare (ignoreable output-type))
+  (let ((parts (split-to-integers text "/")))
+    (make-date (third parts) (first parts) (second parts))))
+
+(defmethod parse ((text string) (output-type (eql :iso-date)) &optional (arg nil))
+  "YYYY-MM-DDT00:00:00 (ignore the time portion)"
+  (declare (ignoreable output-type))
+  (let ((parts (split-to-integers text "-")))
+    (make-date (first parts) (second parts) (third parts))))
+
+(defmethod parse ((text string) (output-type (eql :interval)) &optional (arg nil))
+  "Return a list of intervals parsed from a string of the form '1 year - 4 days'."
+  (declare (ignoreable output-type))
+  (unless (or (string= "" text) (null text))
+    (let ((str2 (ppcre:regex-replace-all "[sS\\s]" text ""))
+          result)
+      (ppcre:do-register-groups ((#'parse-integer value) (#'(lambda (str) (parse str :keyword)) unit))
+                                ("([+-]?\\d+)(\\w+)" str2)
+                                (push (make-interval value unit) result))
+      (nreverse result))))
+
+(defmethod parse ((text string) (output-type (eql :keyword)) &optional (arg 1)) 
+  "Intern a symbol named by the argument into the KEYWORD package.
+
+   If arg is anything other than 1, add an 'S' to the name of the symbol."
+  (declare (ignoreable output-type))
+  (intern (format nil "~:@(~a~p~)" (kebab:to-kebab-case text) arg) :keyword)) 
+
+(defun split-to-integers (text delimiter)
+  "Split the string and return a list of integers."
+  (mapcar #'(lambda (str) (parse-integer str :junk-allowed t)) (ppcre:split delimiter text)))
+
